7ffee089546c0927a8e0344b5e7b62fae6ac7f9c
Kendall Dave <kdaveeffect@yahoo.com>
2022-04-10 21:57:56 -0400

    Initial Scoreboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'scoreboard';
+ public title = 'scoreboard';
+  public startingMinutes = 1;
+  public time = 10;
+  public scoreboardMessage: string;
+  
+  ngOnInit(){
+  setInterval(()=> {
+    if(this.time >= 0){
+    this.updateQuarterGameClock();
+    }
+  }, 1000);
+  
+  }
+
+  updateQuarterGameClock() {
+    const minutes = Math.floor(this.time / 60);
+   
+    let seconds = this.time % 60;
+    seconds = seconds < 10 ? 0 + seconds : seconds;
+this.scoreboardMessage = `${minutes}:${seconds}`;
+   this.time--;
+  }
 }
